e7e2c4c4e0596eb5d5009edc38603e8f3215bbec
lomaka <lomaka13@ukr.net>
2016-10-10 11:05:32 +0300

    initial

--- aes.js
+++ aes.js
@@ -6,6 +6,29 @@
  * @type {exports}
  */
 // encrypt/decrypt functions
+var MergeRecursive = function (obj1, obj2) {
+
+    for (var p in obj2) {
+        try {
+            // Property in destination object set; update its value.
+            if (obj2[p].constructor == Object) {
+                obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+
+            } else {
+                obj1[p] = obj2[p];
+
+            }
+
+        } catch (e) {
+            // Property in destination object not set; create it and set its value.
+            obj1[p] = obj2[p];
+
+        }
+    }
+
+    return obj1;
+}
+
 module.exports = {
 
     /**
@@ -82,26 +105,5 @@ module.exports = {
         return null;
     },
 
-    MergeRecursive: function (obj1, obj2) {
-
-        for (var p in obj2) {
-            try {
-                // Property in destination object set; update its value.
-                if (obj2[p].constructor == Object) {
-                    obj1[p] = MergeRecursive(obj1[p], obj2[p]);
-
-                } else {
-                    obj1[p] = obj2[p];
-
-                }
-
-            } catch (e) {
-                // Property in destination object not set; create it and set its value.
-                obj1[p] = obj2[p];
-
-            }
-        }
-
-        return obj1;
-    }
+    MergeRecursive: MergeRecursive
 };
